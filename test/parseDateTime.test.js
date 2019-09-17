73592fa950e534812930ae5b38f9a2de44cf2d73
bergi <bergi@axolotlfarm.org>
2019-09-17 23:29:04 +0200

    fixed accidentally renamed function in parseDateTime test

--- test/parseDateTime.test.js
+++ test/parseDateTime.test.js
@@ -1,27 +1,27 @@
 /* global describe, it */
 
 const assert = require('assert')
-const parseDateTimeTest = require('../lib/parseDateTime')
+const parseDateTime = require('../lib/parseDateTime')
 
 describe('parseDateTime', () => {
   it('should be a function', () => {
-    assert.strictEqual(typeof parseDateTimeTest, 'function')
+    assert.strictEqual(typeof parseDateTime, 'function')
   })
 
   it('should parse a date time string', () => {
-    const dateTime = parseDateTimeTest('2018-01-01T01:00:00.000+0100')
+    const dateTime = parseDateTime('2018-01-01T01:00:00.000+0100')
 
     assert.strictEqual(dateTime.toUTC().toISO(), '2018-01-01T00:00:00.000Z')
   })
 
   it('should parse a date time string and set the given timezone', () => {
-    const dateTime = parseDateTimeTest('2018-01-01T01:00:00', null, 'Europe/Berlin')
+    const dateTime = parseDateTime('2018-01-01T01:00:00', null, 'Europe/Berlin')
 
     assert.strictEqual(dateTime.toUTC().toISO(), '2018-01-01T00:00:00.000Z')
   })
 
   it('should parse a date time string using the format argument', () => {
-    const dateTime = parseDateTimeTest('20180101 000000', 'yyyyMMdd HHmmss', 'UTC')
+    const dateTime = parseDateTime('20180101 000000', 'yyyyMMdd HHmmss', 'UTC')
 
     assert.strictEqual(dateTime.toUTC().toISO(), '2018-01-01T00:00:00.000Z')
   })
